85e24cfeb5a89d293f49c5933e9f81372a93e1ce
ar4rpon <hsgw28tech@gmail.com>
2025-02-09 17:04:34 +0900

    feat:メモ一覧ページ作成 #28

--- resources/ts/Pages/MemoList.tsx
+++ resources/ts/Pages/MemoList.tsx
@@ -1,6 +1,14 @@
 import MemoCard from '@/Components/MemoCard';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/Components/ui/select';
 import DefaultLayout from '@/Layouts/DefaultLayout';
 import { Head } from '@inertiajs/react';
+import { useState } from 'react';
 
 export default function MemoList() {
   const sampleBook = {
@@ -9,33 +17,68 @@ export default function MemoList() {
     author: '山田太郎',
     coverUrl: 'https://shop.r10s.jp/book/cabinet/9163/9784297129163_1_4.jpg',
   };
+
+  const memoData = [
+    {
+      id: '1',
+      contents: [
+        'サンプルコンテンツ1',
+        'サンプルコンテンツ2',
+        'サンプルコンテンツ3',
+      ],
+      createdAt: '2024-02-02T10:00:00Z',
+      book: sampleBook,
+    },
+    {
+      id: '2',
+      contents: [
+        'サンプルコンテンツ4',
+        'サンプルコンテンツ5',
+        'サンプルコンテンツ6',
+      ],
+      createdAt: '2024-02-02T10:00:00Z',
+      book: sampleBook,
+    },
+    {
+      id: '3',
+      contents: [
+        'サンプルコンテンツ7',
+        'サンプルコンテンツ8',
+        'サンプルコンテンツ9',
+      ],
+      createdAt: '2024-02-02T10:00:00Z',
+      book: sampleBook,
+    },
+  ];
+
+  const [sortBy, setSortBy] = useState('date');
+
   return (
     <DefaultLayout header="メモ一覧">
       <Head title="MemoList" />
-      <div className="rounded-sm border border-green-600 bg-white shadow-md">
-        <h2 className="px-2 py-2 text-xl font-semibold md:px-4 md:py-4 md:text-2xl">
-          最近書いたメモ
-        </h2>
+
+      <div className="mb-4">
+        <Select value={sortBy} onValueChange={setSortBy}>
+          <SelectTrigger className="w-full sm:w-[180px]">
+            <SelectValue placeholder="並び替え" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="date">メモ追加が新しい順</SelectItem>
+            <SelectItem value="title">書籍タイトル順</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
-      <div className="mt-8 grid grid-cols-1 gap-x-8 gap-y-4">
-        <MemoCard
-          id="1"
-          content="サンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツ"
-          createdAt="2024-02-02T10:00:00Z"
-          book={sampleBook}
-        />
-        <MemoCard
-          id="1"
-          content="サンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツ"
-          createdAt="2024-02-02T10:00:00Z"
-          book={sampleBook}
-        />
-        <MemoCard
-          id="1"
-          content="サンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツサンプルコンテンツ"
-          createdAt="2024-02-02T10:00:00Z"
-          book={sampleBook}
-        />
+
+      <div className="grid grid-cols-1 gap-x-8 gap-y-4">
+        {memoData.map((memo) => (
+          <MemoCard
+            key={memo.id}
+            id={memo.id}
+            contents={memo.contents}
+            createdAt={memo.createdAt}
+            book={memo.book}
+          />
+        ))}
       </div>
     </DefaultLayout>
   );
